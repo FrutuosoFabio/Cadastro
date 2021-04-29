e4cb76ea20ce8706a2c367cf6a55185f2e069ce6
FrutuosoFabio <fabio_frutuoso@hotmail.com>
2021-04-29 13:56:14 -0400

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts.app')
+
+
+@section('body')
+    
+
+<div class="jumbotron bg-light border border-secundary">
+    <div class="clas row">
+       <div class="card-deck">
+          <div class="card border bodrder-primary">
+             <div class="card-body">
+                <h5 class="Card-title">Cadastro de produtos</h5>
+                <p class="card-text">
+
+                      Aqui você cadastra todos seus produtos.
+                      Só não se esqueça de cadastrar previamente as categorias
+
+
+                </p>
+                <a href="/produtos"    class="btn btn-primary" >Cadastre seus produtos</a>
+
+             </div>
+           </div>
+
+           <div class="card border bodrder-primary">
+            <div class="card-body">
+               <h5 class="Card-title">Cadastro de categorias</h5>
+               <p class="card-text">
+
+                     Aqui você cadastra todas as suas categorias de produtos
+
+
+               </p>
+               <a href="/categorias"    class="btn btn-primary" >Cadastre sua categorias</a>
+
+            </div>
+          </div>
+        </div>
+      </div>
+</div>
+
+
+
+
+
+
+
+</div>
+
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,61 @@
+<html>
+
+
+<body>
+    <head>
+ <link rel="stylesheet" href="{{asset('css/app.css') }}" >
+ <meta name="csrf-token" content="{{csrf_token()}}"  >
+
+    <style>  body{  padding: 20px}  
+    .navbar{
+      margin-bottom: 20px
+
+    }
+    
+    </style>
+  <title>  Cadastro de produtos</title>
+    </head>
+    
+
+      
+    
+
+    <div class="container">
+        @component('component_navbar')
+        @endcomponent
+
+        <main role="main">
+
+            @hasSection ('body')
+            @yield('body')
+                
+            @else
+                
+            @endif
+        </main>
+
+
+    </div>
+
+
+
+
+
+
+
+
+
+    <script src="{{asset('js/app.js')}}" type="text/javascript" ></script>
+
+</body>
+
+
+
+
+
+
+
+
+
+
+</html>
